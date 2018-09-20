205b33f3775fd10f3260629cde94af1e5ca863ff
Michael Sihombing <michael21sihombing@gmail.com>
2018-09-20 20:55:46 +0700

    Initial commit

---
+++ app/src/main/java/com/pptb/eirene/mapit/Login.java
@@ -0,0 +1,42 @@
+package com.pptb.eirene.mapit;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Login extends AppCompatActivity {
+
+    private EditText Name;
+    private EditText Password;
+    private Button Login;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Name = (EditText)findViewById(R.id.mitUsername);
+        Password = (EditText)findViewById(R.id.mitPassword);
+        Login = (Button)findViewById(R.id.btnLogin);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validate(Name.getText().toString(), Password.getText().toString());
+            }
+        });
+
+    }
+
+    private void validate(String userName, String userPassword){
+        if((userName.equals("admin")) && (userPassword.equals("1234"))){
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+        }
+    }
+
+}
