6b21b9336d342c3d6ae2e0af9dc6c90d2dca990c
Michael Sihombing <michael21sihombing@gmail.com>
2018-09-21 07:53:01 +0700

    Perubahan untuk Verifikasi email

--- app/src/main/java/com/pptb/eirene/mapit/MainActivity.java
+++ app/src/main/java/com/pptb/eirene/mapit/MainActivity.java
@@ -1,13 +1,55 @@
 package com.pptb.eirene.mapit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    private void Logout(){
+        firebaseAuth.signOut();
+        finish();
+        startActivity(new Intent(MainActivity.this, Login.class));
+    }
+
+    private void About(){
+        startActivity(new Intent(MainActivity.this, About.class));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case  R.id.logoutMenu:{
+                Logout();
+            }
+            break;
+            case R.id.aboutMenu:{
+                About();
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
